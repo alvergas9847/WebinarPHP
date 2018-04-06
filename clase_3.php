62282095129745ca2423211c397985742e53dc0d
358b06c6 <eramirezf141@jal.conalep.edu.mx>
2018-04-06 09:42:03 -0500

    Clase 3: Introducción a PHP POO

---
+++ clase_3.php
@@ -0,0 +1,58 @@
+<?php
+/**
+  * Ejercicio:
+  * 
+  * 1. Crear una función que modifique la propiedad $nombreObjeto.
+  * 2. Convertir en privada la propiedad $nombreObjeto.
+  * 3. Crear 5 diferentes objetos con diferente nombre y mostrar los nombres de cada uno.
+  * 
+  * Autor: 358b06c6
+  * Fecha: 31/03/2018
+  * 
+  * Ejercicio de la clase en vivo: https://www.facebook.com/ComunidadDePeHPe/videos/1482443465199112/
+  *
+*/
+class Clase{
+  # Establecer la propiedad nombreObjeto como privada.
+  private $nombreObjeto = '';
+	
+  # Mostrar el nombre del objeto cuando se ejecute.
+  public function __construct(){
+	echo "Se creo el objeto: ";    
+  }
+	
+  # Establecer el nombre del objeto.
+  public function establecerNombreObjeto($nombre){
+	$this->nombreObjeto = $nombre . "\n";
+  }
+	
+  # Obtener el valor de la propiedad $nombreObjeto.
+  public function obtenerNombreObjeto(){
+	echo $this->nombreObjeto;
+  }
+}
+
+# Objeto: 1
+$ObjetoUno = new Clase();
+$ObjetoUno->establecerNombreObjeto('Objeto Uno');
+$ObjetoUno->obtenerNombreObjeto();
+
+# Objeto: 2
+$ObjetoDos = new Clase();
+$ObjetoDos->establecerNombreObjeto('Objeto Dos');
+$ObjetoDos->obtenerNombreObjeto();
+
+# Objeto: 3
+$ObjetoTres = new Clase();
+$ObjetoTres->establecerNombreObjeto('Objeto Tres');
+$ObjetoTres->obtenerNombreObjeto();
+
+# Objeto: 4
+$ObjetoCuatro = new Clase();
+$ObjetoCuatro->establecerNombreObjeto('Objeto Cuatro');
+$ObjetoCuatro->obtenerNombreObjeto();
+
+# Objeto: 5
+$ObjetoCinco = new Clase();
+$ObjetoCinco->establecerNombreObjeto('Objeto Cinco');
+$ObjetoCinco->obtenerNombreObjeto();
